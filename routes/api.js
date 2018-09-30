c3f2fec5b1e3584cf939181be870942c846cdbf5
Ozarion <cozarion@gmail.com>
2018-10-01 03:15:47 +0500

    created the project

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,163 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const axios = require('axios');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.DB;
+
+const stockSchema = new Schema({
+  stock : {type: String, uppercase: true, required: true},
+  likes : [{type: String}]
+});
+
+const Stock = mongoose.model('Stock', stockSchema);
 
 module.exports = function (app) {
+  
+  mongoose.connect(CONNECTION_STRING);
+  const db = mongoose.connection;
+  
+  db.on('error', () => { console.log('Cannot connect to database'); });
+  
+  db.once('open', () => {
+    console.log('successfully connected to database');
+    app.route('/api/stock-prices')
+      .get(handleStockPricesGET);
+    //404 Not Found Middleware
+    app.use(function(req, res, next) {
+      res.status(404)
+        .type('text')
+        .send('Not Found');
+    }); 
+  });
+  
+};
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+const buildRequest = (stock) => {
+  return `https://www.alphavantage.co/query?symbol=${stock}&apikey=${process.env.STOCK_API_KEY}&function=GLOBAL_QUOTE`;
+}
+
+function handleStockPricesGET(req, res) {
+  const clientIps = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  const clientIp = clientIps.split(',')[0];
+  console.log("request coming from ");
+  console.log(clientIp);
+  
+  if (!("stock" in req.query && req.query.stock !== '')) {
+      return res.send('query for a stock dammit!');
+  }
+  const stocks = req.query.stock.toString().split(',');
+  
+  // For one stock
+  if (stocks.length === 1) {
+    axios.get(buildRequest(stocks[0]))
+      .then(response => {
+      // Check if response if empty
+      if (!Object.keys(response.data['Global Quote']).length) {
+        return res.json({"stockData": [], notFound: true});
+      }
+      const stockData = {"stock": response.data['Global Quote']['01. symbol'], 
+                            "price": response.data['Global Quote']['05. price']};
+      // Find this stock in db
+      Stock.findOne({stock: stockData.stock}, (err, doc) => {
+        if (err) {
+          return res.json({"ERROR": err});
+        }
+        // if nothing is found, create new stock
+        const stockDoc = (!doc) ? new Stock({"stock": stockData.stock, "likes": []}) : doc;
+        // if like is passed and there are no likes by this ip then register a like
+        if ("like" in req.query && req.query.like.toLowerCase() === "true" && stockDoc.likes.every(ip => ip !== clientIp)) {
+          stockDoc.likes.push(clientIp);
+        }
+        // save and return response
+        stockDoc.save((err, savedDoc) => {
+          if (err) {
+            return res.json(err);
+          }
+          // RESULT RESPONSE
+          return res.json({"stockData": {"stock": savedDoc.stock, "price": stockData.price, "likes": savedDoc.likes.length}});  
+        });
+      });
+    })
+      .catch(err => {
+      return res.send('Error Occured');
     });
-    
+  } 
+  else if (stocks.length === 2) {
+    // For Two stocks
+    axios.all([
+      axios.get(buildRequest(stocks[0])),
+      axios.get(buildRequest(stocks[1]))
+    ])
+      .then(axios.spread((firstResp, secondResp) => {
+      if (!Object.keys(firstResp.data['Global Quote']).length || !Object.keys(firstResp.data['Global Quote']).length ) {
+        return res.json({"stockData": [], notFound: true});
+      }
+      const stockData = [
+        {
+          "stock": firstResp.data['Global Quote']['01. symbol'], 
+          "price": firstResp.data['Global Quote']['05. price']
+        },
+        {
+          "stock": secondResp.data['Global Quote']['01. symbol'], 
+          "price": secondResp.data['Global Quote']['05. price']
+        }
+      ];
+      Stock.findOne({"stock": stockData[0].stock})
+      .exec((err, firstStock) => {
+        if (err) {
+          return res.json({"ERROR": err});
+        }
+        const firstStockDoc = (!firstStock) ? new Stock({"stock" : stockData[0].stock, "likes": []}) : firstStock;
+        // if like is passed and there are no likes by this ip then register a like
+        if ("like" in req.query && req.query.like.toLowerCase() === "true" && firstStockDoc.likes.every(ip => ip !== clientIp)) {
+          firstStockDoc.likes.push(clientIp);
+        }
+        firstStockDoc.save((err, savedFirstDoc) => {
+          if (err) {
+            return res.json(err);
+          }
+          Stock.findOne({"stock": stockData[1].stock})
+          .exec((err, secondStock) => {
+            if (err) {
+              return res.json({"ERROR": err});
+            }
+            const secondStockDoc = (!secondStock) ? new Stock({"stock": stockData[1].stock, "likes": []}) : secondStock;
+            // if like is passed and there are no likes by this ip then register a like
+            if ("like" in req.query && req.query.like.toLowerCase() === "true" && secondStockDoc.likes.every(ip => ip !== clientIp)) {
+              secondStockDoc.likes.push(clientIp);
+            }
+            secondStockDoc.save((err, savedSecondDoc) => {
+              if (err) {
+                return res.json(err);
+              }
+              const finalResponse = {
+                "stockData": [
+                  {
+                    "stock" : savedFirstDoc.stock, 
+                    "price": stockData[0].price, 
+                    "rel_likes": (savedFirstDoc.likes.length-savedSecondDoc.likes.length)
+                  },
+                  {
+                    "stock": savedSecondDoc.stock,
+                    "price": stockData[1].price,
+                    "rel_likes": (savedSecondDoc.likes.length-savedFirstDoc.likes.length)
+                  }     
+                ]
+              }
+              return res.json(finalResponse);
+            });
+          });
+        });
+      });
+    }))
+      .catch(err => {
+      return res.send('Error Occured');
+    });
+  } 
+  else {
+    return res.send('too many stock queries');
+  }
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,31 +18,71 @@ suite('Functional Tests', function() {
     suite('GET /api/stock-prices => stockData object', function() {
       
       test('1 stock', function(done) {
-       chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-          
-          //complete this one too
-          
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({"stock": "Goog"})
+          .end(function(err, res){
+          assert.equal(res.status, 200, 'response is OK');
+          assert.property(res.body.stockData, "stock", 'has stock property');
+          assert.property(res.body.stockData, "price", 'has price property');
+          assert.property(res.body.stockData, "likes", 'has likes property');
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({"stock": "msft", like: true})
+          .end(function(err, res){
+          assert.equal(res.status, 200, 'response is OK');
+          done();
+          });
       });
       
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+      test('1 stock with like again (ensure likes arent double counted)', function(done){
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({"stock": "msft", like: true})
+          .end(function(err, res){
+          assert.equal(res.status, 200, 'response is OK');
+          let like = res.body.stockData.likes;
+          chai.request(server)
+          .get('/api/stock-prices')
+          .query({"stock": "msft", like: true})
+          .end(function(err, res){
+            assert.equal(res.status, 200, 'response is OK');
+            assert.equal(res.body.stockData.likes, like, 'Like function is good');
+            done();
+          });
+        });
       });
       
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({"stock": ['Goog','Msft']})
+          .end(function(err, res){
+          console.log(res.body);
+          assert.equal(res.status, 200, 'response is OK');
+          assert.isArray(res.body.stockData, 'stock Data is array');
+          assert.equal(res.body.stockData.length, 2, 'Stock data is proper length');
+          done();
+        });
       });
       
       test('2 stocks with like', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({"stock": ['Goog','Msft']})
+          .end(function(err, res){
+          console.log(res.body);
+          assert.equal(res.status, 200, 'response is OK');
+          assert.property(res.body.stockData[0], "rel_likes", 'has rel_likes property');
+          assert.property(res.body.stockData[1], "rel_likes", 'has rel_likes property');
+          assert.equal(res.body.stockData.length, 2, 'Stock data is proper length');
+          done();
+        });
       });
       
     });
